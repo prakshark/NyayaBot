1d49215cf3ff7677c0b883dc3bc8d2249ae5e124
prakshark <prakhar112358@gmail.com>
2024-09-07 08:22:13 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatbtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+const inputInitHeight = chatInput.scrollHeight;
+let currUserMsg = "NULL";
+
+const createChatLi = (message, className) => {
+  //Create a chat <li> element with passed message and class name
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  let chatContent =
+    className === "outgoing"
+      ? `<p></p>`
+      : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi; // return chat <li> element
+};
+
+
+// -------------------------------------------------------------------------------------
+// Main function to generate the message from api call :-
+const generateResponse = (incomingChatli) => {
+  const messageElement = incomingChatli.querySelector("p");
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => {
+        messageElement.textContent = `User msg: ${currUserMsg} API call: ${data.title}`;
+      })
+};
+// -------------------------------------------------------------------------------------------
+
+const handleChat = () => {
+  userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+  currUserMsg = userMessage;
+  if (!userMessage) return;
+
+  // Clear the input textarea and set its height to default
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+
+  // Append the user's message to the chatbox
+  const outgoingChatli = createChatLi(userMessage, "outgoing");
+  chatbox.appendChild(outgoingChatli);
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  setTimeout(() => {
+    // Display "Typing..." message while waiting for the response
+    const incomingChatli = createChatLi("Typing...", "incoming");
+    chatbox.appendChild(incomingChatli);
+    generateResponse(incomingChatli);
+  }, 600);
+};
+
+chatInput.addEventListener("input", () => {
+  // Adjust the height of the input textarea based on its content
+  chatInput.style.height = `${inputInitHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+  // If Enter key is pressed without the Shift key and the window
+  // width is greater than 800px, handle the chat
+  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    e.preventDefault();
+    handleChat();
+  }
+});
+
+sendChatbtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () =>
+  document.body.classList.remove("show-chatbot")
+);
+chatbotToggler.addEventListener("click", () =>
+  document.body.classList.toggle("show-chatbot")
+);
